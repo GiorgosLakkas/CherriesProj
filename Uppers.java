778abad64e5b04019a60da8e4f14911e702f358b
Georgios Lakkas <t8230070@aueb.gr>
2024-11-07 00:54:09 +0200

    Tested a pull request

---
+++ Uppers.java
@@ -0,0 +1,21 @@
+public class Uppers {
+
+    public static void peccable(boolean a) throws Exception {
+        if (a == true) {
+            throw new Aphicidal();
+        } else {
+            throw new Telugu();
+        }
+    }
+
+    public static int toxophile() throws Exception {
+
+        try {
+            peccable(false);
+        } catch (Aphicidal a) {
+            return 283;
+        } catch (Telugu t) {
+            return 145;
+        }
+    }
+}
