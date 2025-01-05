b2d7939dda5f7741e719de33b6ed9b357aebae5a
Georgios Lakkas <t8230070@aueb.gr>
2025-01-05 23:13:58 +0200

    Modified the MatchAlgoFriend class

--- src/main/java/com/example/unimeets/MatchAlgoFriend.java
+++ src/main/java/com/example/unimeets/MatchAlgoFriend.java
@@ -1,70 +1,87 @@
 package com.example.unimeets;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MatchAlgoFriend {
-    private  String[] User1 = new String[22];
-    private String[] User2 = new String[22];
 
-    
-     public MatchAlgoFriend() {
-        // Initialize User1 and User2 with default values
-        FilterCriteria userinfo = new FilterCriteria(null, null, null, null);
-        User1[0] = userinfo.getUniversityName();
-        User1[1] = userinfo.getDepartment();
-        User1[2] = String.valueOf(userinfo.getAge());
-        User1[3] = userinfo.getGender();
+    private String[] User1 = new String[22];
+
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+
+    public MatchAlgoFriend(UserProfile userProfile) {
+        User1[0] = userProfile.getUniversity();
+        User1[1] = userProfile.getDepartment();
+        User1[2] = String.valueOf(userProfile.getAge());
+        User1[3] = userProfile.getGender();
+        List<String> interests = userProfile.getInterests();
+        for (int i = 0; i < interests.size(); i++) {
+            User1[4 + i] = interests.get(i);
+        }
+    }
+
+    public List<String> getUsersAbove95Match() {
+        List<UserProfile> allUsers = userProfileRepository.findAll();
+        List<String> highMatchUsers = new ArrayList<>();
+
+        for (UserProfile otherUser : allUsers) {
+            if (otherUser.getMyAppUser().getUsername().equals(User1[4])) {
+                continue; 
+            }
+
+            String[] User2 = new String[22];
+            User2[0] = otherUser.getUniversity();
+            User2[1] = otherUser.getDepartment();
+            User2[2] = String.valueOf(otherUser.getAge());
+            User2[3] = otherUser.getGender();
+            List<String> interests = otherUser.getInterests();
+            for (int i = 0; i < interests.size(); i++) {
+                User2[4 + i] = interests.get(i);
+            }
+
+            double matchInterest = CalculateMatchInterest(User1, User2);
+            matchInterest = AdaptMatchInterest(matchInterest);
+            double matchBase = CalculateMatchBase(User1, User2);
+            double finalMatch = 0.5 * matchInterest + 0.5 * matchBase;
+
+            if (finalMatch >= 0.95) {
+                highMatchUsers.add(otherUser.getMyAppUser().getName() + " - Match: " + (finalMatch * 100) + "%");
+            }
+        }
+        return highMatchUsers;
+    }
+
+    public double CalculateMatchInterest(String[] User1, String[] User2) {
+        double matchInterest = 0;
+        for (int i = 4; i < User1.length; i++) {
+            if (User1[i] != null && User1[i].equals(User2[i])) {
+                matchInterest++;
+            }
+        }
+        return matchInterest / 18;
+    }
+
+    public double AdaptMatchInterest(double matchInterest) {
+        if (matchInterest < 0.3) {
+            matchInterest -= 0.1 * matchInterest;
+        } else if (matchInterest < 0.7) {
+            matchInterest += 0.1 * matchInterest;
+        } else {
+            matchInterest += 0.2 * matchInterest;
+        }
+        return Math.min(1.0, Math.max(0, matchInterest));
+    }
+
+    public double CalculateMatchBase(String[] User1, String[] User2) {
+        if (User1[1].equals(User2[1]) && User1[2].equals(User2[2])) {
+            return 1;
+        } else if (User1[1].equals(User2[1])) {
+            return 0.5;
+        }
+        return 0;
+    }
 }
-    
-    
-    double matchinterest = 0;
 
-
-    //Count the same interests the 2 users have
-    public double  CalculateMatchInterest(String[] User1,String[] User2){
-    for (int i=4;i<User1.length; i++){
-        if (User1[i].equals(User2[i])){
-            matchinterest++;
-        }
-    }
-    
-    matchinterest=matchinterest/18;
-    return matchinterest;
-    }
-
-    //Adjust the percentage of the matched interests
-    public double AdaptMatchInterest(double matchinterest){
-        if (matchinterest<0.3){
-            matchinterest=matchinterest-0.1*matchinterest;
-        }else if(matchinterest<0.7){
-            matchinterest=matchinterest+0.1*matchinterest;
-        }else{
-            matchinterest=matchinterest+0.2*matchinterest;
-        }
-        if (matchinterest>1.0){
-            matchinterest=1.0;
-        }
-        if (matchinterest<0){
-            matchinterest=0;
-        }
-        return matchinterest;
-    }
-
-    double matchbase = 0;
-
-    //Check whether the users are from the same school and departemnt
-    
-    public double  CalculateMatchBase(String[] User1,String[] User2){
-    if (User1[1].equals(User2[1]) && User1[2].equals(User2[2])){
-        matchbase=1;
-    }else if(User1[1].equals(User2[1])){
-        matchbase=0.5;
-    }else{
-        matchbase=0;
-    }
-    return matchbase;
-    }
-
-    public double CalculateMatch(){
-     double match=0.5*matchbase+0.5*matchinterest;
-     return match;
-    }
-}
